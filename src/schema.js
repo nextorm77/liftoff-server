dda2beb0d9fc9dd4b8f63e0ce505da7179ff0856
nextorm77 <sonsw77@gmail.com>
2022-11-05 12:07:08 +0900

    initialized

---
+++ src/schema.js
@@ -0,0 +1,60 @@
+import { gql } from "apollo-server";
+
+export const typeDefs = gql`
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, respresents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+  type Mutation {
+    "Increment the number of views of a given track, when the track card is clicked"
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse
+  }
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
+  }
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The Track's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+    "The Track's complete description, can be in Markdown foramt"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "Thr Track's complete array of Modules"
+    modules: [Module!]!
+  }
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
+`;

e38417a158333e914d5c7e5b1de830028d9751b5
nextorm77 <sonsw77@gmail.com>
2022-11-07 23:32:57 +0900

    add a Query.module

--- src/schema.js
+++ src/schema.js
@@ -20,6 +20,8 @@ export const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module!
   }
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
@@ -60,5 +62,9 @@ export const typeDefs = gql`
     length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's video duration, in seconds"
     durationInSeconds: Int
+    "The module's text-based description, can be in markdown format. In case of a video, it weill be the enriched transcript"
+    content: String
+    "The module's video url, for video-based modules"
+    videoUrl: String
   }
 `;
